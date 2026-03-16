218f1e634934e759ddeb3642089d9714c4289f46
v4mpcrw <gloxinias.in.my.crypt@gmail.com>
2026-03-16 00:13:35 -0300

    estructura base

--- Running_for_gold.cpp
+++ Running_for_gold.cpp
@@ -1,1 +1,25 @@
 //https://codeforces.com/group/ibNhxWfOek/contest/341358/problem/B
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; cin>>t;
+
+    while(t--)
+    {
+        int n; cin>>n;
+
+        for(int i = 0; i < n; i++)
+        {
+            int R[n][5]; //r[i][j], n°atleta y n°maraton
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    cin>>R[i][j];
+                }
+            }
+        }
+    }
+
+}
